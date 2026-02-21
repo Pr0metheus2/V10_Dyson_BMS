7d8b47240c4edb99d931ad5ba0fbc8c5d82ac73a
David Pye <davidmpye@gmail.com>
2026-02-21 19:49:40 +0000

    Update asf config

--- V10_BMS/src/asf.h
+++ V10_BMS/src/asf.h
@@ -124,4 +124,11 @@
 // From module: SYSTEM - Reset Management for SAM D20/D21/R21/D09/D10/D11/DA/HA
 #include <reset.h>
 
+// From module: TC - Timer Counter (Polled APIs)
+#include <tc.h>
+#include <tc_interrupt.h>
+
+// From module: WDT - Watchdog Timer (Polled APIs)
+#include <wdt.h>
+
 #endif // ASF_H
